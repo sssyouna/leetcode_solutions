c15f95fd1a99623d0b0e06a8055fa99b9c9fe00b
Younass Ibn abdeslam <younessibnabdeslam@gmail.com>
2026-08-04 15:00:32 +0100

    Time: 0 ms (100.00%) | Memory: 10.6 MB (72.30%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.c
+++ 88-merge-sorted-array/merge-sorted-array.c
@@ -1,26 +1,20 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int k=m;
-if(m==0){
-    for(int i=0;i<n;i++){
-       nums1[i]=nums2[i];  
+    int i = m - 1;   
+int j = n - 1;      
+int k = m + n - 1; 
+while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+        nums1[k] = nums1[i];
+        i--;
+    } else {
+        nums1[k] = nums2[j];
+        j--;
     }
-return ;}
-
-    for(int i=0;i<n;i++){
-        for(int j=k-1;j>=0;j--){
-            
-            if(nums2[i]<nums1[j]){
-                nums1[j+1]=nums1[j];
-            }
-            else{
-                nums1[j+1]=nums2[i];
-                break;
-            }
-             if (j == 0) {
-        nums1[0] = nums2[i];
-    }
-        }
-        k++;
-        }
-    }
-    
+    k--;
+}
+while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+}
+}
