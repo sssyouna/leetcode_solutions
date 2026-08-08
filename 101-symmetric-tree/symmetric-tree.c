bf2c459629e671b831a0b47e06757e7cd8ac3ece
Younass Ibn abdeslam <younessibnabdeslam@gmail.com>
2026-08-08 18:36:27 +0100

    Time: 0 ms (100.00%) | Memory: 11.2 MB (3.93%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.c
+++ 101-symmetric-tree/symmetric-tree.c
@@ -7,16 +7,45 @@
  * };
  */
 
- bool isMirror(struct TreeNode* left,struct TreeNode* right){
-     if (left==NULL && right ==NULL) return true;
-    if(left ==NULL || right==NULL) return false;
-    if(left->val!= right->val) return false;
-    return isMirror(left->right,right->left)&&isMirror(left->left,right->right);
-    
- }
 bool isSymmetric(struct TreeNode* root) {
-   return isMirror(root->left,root->right);
 
+    if (root == NULL)
+        return true;
 
-    
+    struct TreeNode *stack1[1000];
+    struct TreeNode *stack2[1000];
+
+    int top = 0;
+
+    stack1[top] = root->left;
+    stack2[top] = root->right;
+    top++;
+
+    while (top > 0) {
+
+        top--;
+
+        struct TreeNode *left = stack1[top];
+        struct TreeNode *right = stack2[top];
+
+        if (left == NULL && right == NULL)
+            continue;
+
+        if (left == NULL || right == NULL)
+            return false;
+
+        if (left->val != right->val)
+            return false;
+
+        // Mirror pairs
+        stack1[top] = left->left;
+        stack2[top] = right->right;
+        top++;
+
+        stack1[top] = left->right;
+        stack2[top] = right->left;
+        top++;
+    }
+
+    return true;
 }
