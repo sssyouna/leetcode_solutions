c90d8f61d6d3f317c0ee96db47d9434c7f001070
Younass Ibn abdeslam <younessibnabdeslam@gmail.com>
2026-08-13 23:06:39 +0100

    Time: 8 ms (88.16%) | Memory: 81 MB (79.40%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int minDepth(struct TreeNode* root) {
+    if (root==NULL) return 0;
+
+    int left= minDepth(root->left);
+    int right=minDepth(root->right);
+    
+    if (right==0 ) return left +1;
+    if (left ==0) return right +1;
+    return (left>right ? right :left )+1;
+  
+    
+}
